ea52aac1b47c8c83624540bb6d92718091b9a0db
MatiasCoria <matiascoriaof@gmail.com>
2024-06-14 05:18:23 -0300

    cypress actualizado

--- test/cypress/e2e/testing.cy.js
+++ test/cypress/e2e/testing.cy.js
@@ -1,14 +1,22 @@
-describe("Mi primer test", () => {
-  it("Gets, types and asserts", () => {
-    cy.visit("https://example.cypress.io");
-    cy.contains("type").click();
-    cy.url().should("include", "/commands/actions");
-    cy.get(".action-email").type("fake@email.com");
-    cy.get(".action-email").should("have.value", "fake@email.com");
+describe("Testeando gestion de turnos en Render", () => {
+  it("Sacar un turno", () => {
+    cy.visit("https://gestiondeturnos.onrender.com/");
+    cy.wait(1000);
+    cy.get("#especialidades").select("Odontologia");
+    cy.get("#datepicker").click();
+    cy.contains(".flatpickr-day", 26).click();
+    cy.get("#profesionales").select('Lucas Garcia');
+    cy.get("#btn-submit").click();
   });
-  it("otro test", () => {
-    cy.visit("https://www.uno.edu.ar/");
-    cy.get("#mod-search-searchword").type("mesa");
-    cy.get("input[type='submit']");
-  });
+  it("Modificar un turno", ()=> {
+    cy.visit("https://gestiondeturnos.onrender.com/");
+    cy.contains("a", "Mis turnos").click()
+    cy.get(".btn-modificar").click()
+    cy.wait(500);
+    cy.get("#datepicker").click();
+    cy.get(".flatpickr-next-month").find('svg').click();
+    cy.contains(".flatpickr-day", 2).click();
+    cy.get("#profesionales").select('Marcela Balcarce');
+    cy.get("#btn-submit2").click();
+  })
 });
